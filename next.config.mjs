c6a17280dba6b515cc81be541a278f914009b924
YogurtFusion <cocaniket2@gmail.com>
2026-02-14 14:09:58 +0530

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  // This tells Next.js: "I trust my phone accessing this IP"
+  allowedDevOrigins: ["http://192.168.29.10:3000", "http://localhost:3000"],
 };
 
 export default nextConfig;
