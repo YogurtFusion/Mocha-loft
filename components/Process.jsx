3b248e5ee3a915b930a43ce132322c7a7e6d82f8
YogurtFusion <cocaniket2@gmail.com>
2026-02-16 17:20:47 +0530

    midway of process

--- components/Process.jsx
+++ components/Process.jsx
@@ -1,11 +1,45 @@
 import React from "react";
+import ProcessCard from "./UI/ProcessCard";
+import BrowseIcon from "./UI/BrowseIcon";
+import MessageCloud from "./UI/MessageCloud";
+import OrderIcon from "./UI/OrderIcon";
 
+const cardBody = [
+  {
+    num: 1,
+    title: "Browse Menu",
+    desc: "Explore our curated selection of multi-cuisine favorites",
+    svg: <BrowseIcon/>,
+    
+  },
+  {
+    num: 2,
+    title: "Click Order",
+    desc: "Select your dishes and add them to your order",
+    svg: <MessageCloud/>
+  },
+  {
+    num: 3,
+    title: "Confirm on WhatsApp",
+    desc: "Finalize your order through a quick WhatsApp cha",
+    svg: <OrderIcon/>
+  },
+];
 const Process = () => {
   return (
     <section className="">
       {/* wrap */}
       <div className="">
-        
+        <h1 className="text-center text-primary text-3xl/relaxed leading-16  font-playfair ">
+          How It Works
+        </h1>
+        {/* cards */}
+        <div className="p-6">{
+          cardBody.map((item)=>(
+            <ProcessCard key={item.num}
+            />
+          ))
+          }</div>
       </div>
     </section>
   );
---
+++ components/UI/ProcessCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Cloud } from "lucide-react";
+
+const ProcessCard = (title, desc, num) => {
+  return (
+    <div className="relative bg-gold/15 h-fit rounded-2xl border-[0.5px] border-gold  hover:border-gold-hover transition-colors  ">
+      {/* Wrap */}
+      <div className=" flex justify-center items-center flex-col gap-5 p-10 pt-12  text-center">
+        {/* num */}
+        <div className="absolute  -top-5 left-1/2 -translate-x-1/2 flex justify-center items-center  bg-overlay-black rounded-full border border-gold text-gold w-10 h-10 text-center shadow-md text-base ">
+          {num}
+        </div>
+
+        <div className="bg-gold/30  text-gold p-4  rounded-full  ">
+          <Cloud className=" w-7 h-7 stroke-1  " />
+        </div>
+
+        <h2 className="text-primary text-2xl leading-relaxed font-playfair font-semibold ">
+          {title}
+        </h2>
+        <p className="text-secondary text-sm font-poppins  font-semibold max-w-[200px] ">
+          {desc}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ProcessCard;
